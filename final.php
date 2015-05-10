67bb547f9cd435b4b73c41d2b46987f425fc392b
GodDamnVel <ve23@njit.edu>
2015-05-10 15:19:52 -0400

    Update final.php

--- final.php
+++ final.php
@@ -2,26 +2,33 @@
 <body>
 <?php
 
-//starting connection
- $dbc = mysql_connect('localhost','root','redline9k');
-    if(!$dbc)
-    {
-      die('not connected'   . mysql_error());
-    }
+//singleton connection
+class dbaccess {
 
-//selecting database
+private $connection;
 
-$dbs = mysql_select_db("employees",$dbc);
-   if(!$dbs)
-   {
-     die('no databse ' . mysql_error());
-   }
+public static function getInstance(){
+  static $instance = null;
+  if (null === $instance){
+        $instance = new dbaccess();
+        $instance->connect();
+ }
+  return $instance;
 
-//testing update
+}
 
+private function connect(){
+ $this->connection = new PDO('mysql:host=' .constants::$sqlhost . 'dbname' . constants::$sqldb, constants::$sqldb, constants::$sqlpass);
+}
 
+public function result($query){
 
+ $stmt = $this->connection->prepare($query);
+ $stmt->execute();
+ $result = $stmt->fetch();
+ return $result;
 
-?>
+ }
+}
 </body>
 </html>
